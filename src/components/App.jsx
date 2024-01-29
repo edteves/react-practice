4855d6814633119d3d39c7474f22800c15d53a2b
edteves <edwinjrteves29@gmail.com>
2024-01-29 17:52:05 +0800

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,24 @@
 import { RecipeList } from "./RecipeList/RecipeList";
 import recipes from "../data/recipes.json";
+import { Component } from "react";
 
-export const App = () => {
-	return <RecipeList recipes={recipes} />;
-};
+// class component
+export class App extends Component {
+	handleClick = () => {
+		console.log("hello Im clicked");
+	};
+
+	render() {
+		return (
+			<>
+				<RecipeList recipes={recipes} />
+				<button onClick={this.handleClick}>Click Me</button>
+				<button onClick={() => this.handleClick}>Click Me</button>
+			</>
+		);
+	}
+}
+// functional component - modern react class
+// export const App = () => {
+// 	return <RecipeList recipes={recipes} />;
+// };
