a89d843848164e96c435d478deaa155e34f24506
edteves <edwinjrteves29@gmail.com>
2024-02-15 20:21:40 +0800

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,27 @@
-import { UserMenu } from './UserMenu';
+import { Route, Routes } from 'react-router-dom';
+import { AboutPage } from '../pages/AboutPage/AboutPage';
+import { HomePage } from '../pages/HomePage/HomePage';
+import { ProductDetailsPage } from '../pages/ProductDetailsPage/ProductDetailsPage';
+import { ProductsPage } from '../pages/ProductsPage/ProductsPage';
+import { Mission } from './Mission/Mission';
+import { Team } from './Team/Team';
+import { Reviews } from './Reviews/Reviews';
+import { SharedLayout } from './SharedLayout/SharedLayout';
 
 export const App = () => {
-  return <UserMenu />;
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        {/* A special index props is passed, which tells the React Router that the route index and must be rendered to the same address as its parent. */}
+        <Route index element={<HomePage />} />
+        <Route path="about" element={<AboutPage />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="products/:id" element={<ProductDetailsPage />} />
+      </Route>
+    </Routes>
+  );
 };
