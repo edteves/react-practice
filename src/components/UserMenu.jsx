22b02d6cb6b2f0eecb23fe020b52220f6edd6068
edteves <edwinjrteves29@gmail.com>
2024-03-06 15:55:01 +0800

    update

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -2,15 +2,15 @@ import { useUser } from "../contexts/UserContext";
 
 export const UserMenu = () => {
 	// the custom hook we created to get the value from our context
-	const { isLoggedIn, username, logIn, logOut } = useUser();
+	const { isLoggedIn, username, handleLogin, handleLogout } = useUser();
 
 	return (
 		<div>
 			{isLoggedIn && <p>{username}</p>}
 			{isLoggedIn ? (
-				<button onClick={logOut}>Log out</button>
+				<button onClick={handleLogout}>Log out</button>
 			) : (
-				<button onClick={logIn}>Log in</button>
+				<button onClick={handleLogin}>Log in</button>
 			)}
 		</div>
 	);
